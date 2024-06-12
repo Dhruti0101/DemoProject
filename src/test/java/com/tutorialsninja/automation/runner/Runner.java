6c385d1c148a116ea0b765dc9ef891036ca6b9ad
Dhruti <jobsfordhruti@gmail.com>
2024-06-13 01:16:42 +0530

    Adding first scenario for registration of user

--- src/test/java/com/tutorialsninja/automation/runner/Runner.java
+++ src/test/java/com/tutorialsninja/automation/runner/Runner.java
@@ -1,5 +1,15 @@
 package com.tutorialsninja.automation.runner;
 
-public class Runner {
+import org.junit.runner.RunWith;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features={"classpath:FeatureFiles/Register.feature"},
+glue={"classpath:com.tutorialsninja.automation.stepdef"},
+plugin={"html:target/cucumber_html_report/index.html"},
+tags = "@Register and @One")
+public class Runner {
+      
 }
